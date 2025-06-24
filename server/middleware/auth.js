98f49e21cf9ed7b8fd5198343b0814d5ab54f9c0
Ranjeet Singh <singhranjeet1303@gmail.com>
2025-06-24 20:23:10 +0530

    update authProvider

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,20 +7,20 @@ export const protectRoute = async (req, res, next) => {
     const token = req.headers.token;
 
     if (!token) {
-      return res.status(401).json({ success: false, message: "No token provided" });
+      return res.json({ success: false, message: "No token provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId).select("-password"); // ✅ Fixed here
+    const user = await User.findById(decoded.userId).select("-password");
 
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
+      return res.json({ success: false, message: "User not found" });
     }
 
     req.user = user;
     next();
   } catch (err) {
     console.log("protectRoute error:", err.message);
-    res.status(401).json({ success: false, message: "Invalid token" });
+    res.json({ success: false, message: "Invalid token" });
   }
 };
